693d7196a40346ed0633fee230976ae1c5d29afa
demo_knyang <demo.knyang@gmail.com>
2022-01-07 09:47:36 +0800

    change layout shortcuts to default

--- cfg/config.h
+++ cfg/config.h
@@ -49,19 +49,19 @@ static int resizehints = 1;    /* 1 means respect size hints in tiled resizals *
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[]=",      tile },    /* first entry is default */
-	{ "[M]",      monocle },
-	{ "[@]",      spiral },
-	{ "[\\]",     dwindle },
-	{ "H[]",      deck },
-	{ "TTT",      bstack },
+	{ "[]=",      tile },    /* first entry is default */ /* Master on left, slaves on right */
+	{ "[M]",      monocle },    /* All windows on top of eachother */
+	{ "[@]",      spiral },    /* Fibonacci spiral */
+	{ "[\\]",     dwindle },    /* Decreasing in size right and leftward */
+	{ "H[]",      deck },    /* Master on left, slaves in monicle-like mode on right */
+	{ "TTT",      bstack },    /* Master on top, slaves on bottom */
 	{ "===",      bstackhoriz },
-	{ "HHH",      grid },
+	{ "HHH",      grid },    /* Master and slaves arranged in equal sizes */
 	{ "###",      nrowgrid },
 	{ "---",      horizgrid },
 	{ ":::",      gaplessgrid },
-	{ "|M|",      centeredmaster },
-	{ ">M>",      centeredfloatingmaster },
+	{ "|M|",      centeredmaster },    /* Master in middle, slaves on sides */
+	{ ">M>",      centeredfloatingmaster },    /* Master in middle as float, slaves on sides */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ NULL,       NULL },
 };
@@ -142,9 +142,9 @@ static Key keys[] = {
 	{ MODKEY|Mod1Mask|ShiftMask,    XK_0,      defaultgaps,    {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} }, /* tile */
+	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[13]} }, /* float */
+	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[1]} }, /* monocle */
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY|ShiftMask,             XK_f,      togglefullscr,  {0} },
