bde72875f884b3e04d8732400ad8798bc13fb417
demo_knyang <demo.knyang@gmail.com>
2025-03-30 01:26:11 +0800

    bump version to 6.4

--- cfg/config.h
+++ cfg/config.h
@@ -1,13 +1,13 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
+static unsigned int borderpx        = 1;        /* border pixel of windows */
+static unsigned int snap            = 32;       /* snap pixel */
 static unsigned int gappih          = 20;       /* horiz inner gap between windows */
 static unsigned int gappiv          = 10;       /* vert inner gap between windows */
 static unsigned int gappoh          = 10;       /* horiz outer gap between windows and screen edge */
 static unsigned int gappov          = 30;       /* vert outer gap between windows and screen edge */
 static int smartgaps                = 0;        /* 1 means no outer gap when there is only one window */
-static unsigned int borderpx        = 1;        /* border pixel of windows */
-static unsigned int snap            = 32;       /* snap pixel */
 static int showbar                  = 1;        /* 0 means no bar */
 static int topbar                   = 1;        /* 0 means bottom bar */
 static int viewontag                = 1;        /* 1 means switch view on tag switch */
@@ -44,13 +44,14 @@ static const Rule rules[] = {
 static float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static int nmaster     = 1;    /* number of clients in master area */
 static int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
 #define FORCE_VSPLIT 1  /* nrowgrid layout: force two clients to always split vertically */
 #include "vanitygaps.c"
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[]=",      tile },    /* first entry is default */ /* Master on left, slaves on right */
+	{ "[]=",      tile },    /* first entry is default, master on left, slaves on right */
 	{ "[M]",      monocle },    /* All windows on top of eachother */
 	{ "[@]",      spiral },    /* Fibonacci spiral */
 	{ "[\\]",     dwindle },    /* Decreasing in size right and leftward */
@@ -113,10 +114,12 @@ ResourcePref resources[] = {
     { "gappoh",          	INTEGER, &gappoh },
     { "gappov",          	INTEGER, &gappov },
     { "smartgaps",          INTEGER, &smartgaps },
+    { "viewontag",          INTEGER, &viewontag },
+    { "lockfullscreen",     INTEGER, &lockfullscreen },
     { "scratchpadgeometry", STRING,  &scratchpadgeometry },
 };
 
-static Key keys[] = {
+static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
@@ -128,6 +131,9 @@ static Key keys[] = {
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
+	{ MODKEY|ShiftMask,             XK_h,      setcfact,       {.f = +0.25} },
+	{ MODKEY|ShiftMask,             XK_l,      setcfact,       {.f = -0.25} },
+	{ MODKEY|ShiftMask,             XK_o,      setcfact,       {.f =  0.00} },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY|Mod1Mask,              XK_u,      incrgaps,       {.i = +1 } },
 	{ MODKEY|Mod1Mask|ShiftMask,    XK_u,      incrgaps,       {.i = -1 } },
